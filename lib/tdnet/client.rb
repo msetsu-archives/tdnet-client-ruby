47301ff5a67ea625b2d7d84a460c2398c91d99b5
Katsuma Ito <katsumai@gmail.com>
2016-07-27 17:31:18 +0900

    Client#feeds accept URL string.

--- lib/tdnet/client.rb
+++ lib/tdnet/client.rb
@@ -8,9 +8,15 @@ module TDnet
       @logger ||= options[:logger] || TDnet::Logger.default
     end
 
-    def feeds(date = Date.today, options = {})
-      page = options.delete :page
-      resp = agent.get path(date, page || 0), options
+    def feeds(date_or_url = Date.today, options = {})
+      path = case date_or_url
+               when Date
+                 page = options.delete :page
+                 path(date_or_url, page || 0)
+               else
+                 date_or_url
+             end
+      resp = agent.get path, options
       return unless resp.success?
       resp_ = resp.to_hash
       TDnet::Response::Feed.new(resp_[:body], resp_[:url].to_s)
--- test/tdnet/client_test.rb
+++ test/tdnet/client_test.rb
@@ -26,6 +26,10 @@ class TDnet::ClientTest < Minitest::Test
       refute_nil feed
       assert_equal '24620', feed[:code]
       assert_equal Time.new(2016, 7, 13, 15, 0, 0, '+09:00'), feed[:time]
+
+      feeds = client.feeds('https://www.release.tdnet.info/inbs/I_list_001_20160713.html')
+      refute_nil feeds
+      assert_equal 100, feeds.count
     end
   end
 end
